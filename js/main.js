036faf0d1add9a46c29acfc2ace7db210e2943fe
opezdola <nekras.2001@mail.ru>
2024-02-06 18:07:41 +0300

    4

--- js/main.js
+++ js/main.js
@@ -9,6 +9,28 @@ Vue.component('notes', {
             column1Locked: false,
         };
     },
+    template: `
+    <div class="kanban-board">
+      <div v-for="column in columns" :key="column.id" class="kanban-column">
+        <h2>{{ column.title }}</h2>
+        <button @click="addCard(column)" :disabled="column.id === 1 ? column1Locked : false">Добавить карточку</button>
+        <div v-for="card in column.cards" :key="card.title" class="kanban-card">
+          <h3>{{ card.title }}</h3>
+          <ul>
+            <li v-for="(item, index) in card.items" :key="index" class="kanban-item">
+              <input type="checkbox" v-model="item.completed" @change="moveCardIfCompleted(card)" :disabled="isCardLocked(card)">
+              {{ item.text }}
+              <button @click="removeItem(card, index)" :disabled="isCardLocked(card)">Удалить пункт</button>
+            </li>
+          </ul>
+          <button @click="addItem(card)" :disabled="isCardLocked(card)">Добавить пункт</button>
+          <button @click="removeCard(column, card)" :disabled="isCardLocked(card)">Удалить карточку</button>
+          <p v-if="card.completedAt">Последний пункт выполнен: {{ card.completedAt }}</p>
+        </div>
+      </div>
+    </div>
+  `,
+
 });
 
 new Vue({
