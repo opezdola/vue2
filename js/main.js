ecae1d46f2f513964b6d5df1b2ac660473c58b9f
opezdola <nekras.2001@mail.ru>
2024-02-06 18:12:11 +0300

    5

--- js/main.js
+++ js/main.js
@@ -9,6 +9,49 @@ Vue.component('notes', {
             column1Locked: false,
         };
     },
+    methods: {
+        addCard(column) {
+            if (!this.column1Locked && column.cards.length < this.getMaxCards(column.id)) {
+                const newCard = {title: 'Новая карточка', items: [], columnId: column.id};
+                column.cards.push(newCard);
+
+                if (column.id === 1) {
+                    const completedCount = newCard.items.filter(item => item.completed).length;
+                    const totalCount = newCard.items.length;
+                    if (completedCount / totalCount > 0.5 && this.columns[1].cards.length < this.getMaxCards(2)) {
+                        this.moveCard(newCard, 1, 2);
+                    }
+                }
+            }
+        },
+        removeCard(column, card) {
+            const index = column.cards.indexOf(card);
+            if (index !== -1) {
+                column.cards.splice(index, 1);
+            }
+        },
+        getMaxCards(columnId) {
+            return columnId === 1 ? 3 : columnId === 2 ? 5 : Infinity;
+        },
+        addItem(card) {
+            if (card.items.length < 5) {
+                card.items.push({text: 'Новый пункт', completed: false});
+            }
+        },
+        removeItem(card, itemIndex) {
+            card.items.splice(itemIndex, 1);
+        },
+        moveCard(card, fromColumnId, toColumnId) {
+            const fromColumn = this.columns.find(column => column.id === fromColumnId);
+            const toColumn = this.columns.find(column => column.id === toColumnId);
+            const index = fromColumn.cards.indexOf(card);
+            if (index !== -1) {
+                fromColumn.cards.splice(index, 1);
+                card.columnId = toColumnId;
+                toColumn.cards.push(card);
+            }
+        },
+    },
     template: `
     <div class="kanban-board">
       <div v-for="column in columns" :key="column.id" class="kanban-column">
