f32b53fd18afd920d5f26da80438a52ec544d61b
opezdola <nekras.2001@mail.ru>
2024-02-06 18:04:55 +0300

    3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+Vue.component('notes', {
+    data() {
+        return {
+            columns: [
+                { id: 1, title: 'Подготовка', cards: [] },
+                { id: 2, title: 'В процессе', cards: [] },
+                { id: 3, title: 'Завершенные', cards: [] },
+            ],
+            column1Locked: false,
+        };
+    },
+});
+
 new Vue({
     el: '#app',
 });
